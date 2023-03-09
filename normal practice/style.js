44e8f02fc19171cf35f49b2bca2f2bb9f74fec26
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-03-09 12:20:50 +0600

    My Js Projects

---
+++ normal practice/style.js
@@ -0,0 +1,8 @@
+const de = document.getElementById('popup');
+const hd = document.getElementById('hd');
+
+de.addEventListener('click',function(){
+    
+    hd.className ='d-block';
+
+})
---
+++ todoApp/app.js
@@ -0,0 +1,5 @@
+const do_form = document.getElementById('do_form');
+
+do_form.addEventListener('submit' , function(e){
+    e.preventDefault();
+})
