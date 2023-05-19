3c84b9301242272d54692d8ba0aa80bbadee9bc2
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-05-19 15:52:11 +0600

    localstorage

---
+++ localStorage/app.js
@@ -0,0 +1,26 @@
+const students =[
+    {
+        id : 1,
+        name : 'John',
+        age : 21,
+        skill : 'javascript'
+    },
+    {
+        id : 2,
+        name : 'Nayeem',
+        age : 23,
+        skill : 'React'
+    },
+    {
+        id : 3,
+        name : 'Fahim',
+        age : 16,
+        skill : 'Java'
+    },
+];
+
+
+localStorage.setItem();
+
+
+
